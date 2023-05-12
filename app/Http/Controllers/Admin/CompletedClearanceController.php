a0dc6561001adb51946280e5c21c5e56a0a7dabd
Aldwin Seboguero <aldwin.seboguero@parsu.edu.ph>
2023-05-12 09:08:14 +0800

    Fix Approved Requests Module

--- app/Http/Controllers/Admin/CompletedClearanceController.php
+++ app/Http/Controllers/Admin/CompletedClearanceController.php
@@ -83,10 +83,27 @@ class CompletedClearanceController extends Controller
         }
          else if(Auth::user()->hasRole("pd")){
             $per_page =$request->per_page ? $request->per_page : 10; 
-            $signatory_ids = SignatoryV2::where('user_id',Auth::user()->id)->get('id');
+            $semester = Semester::orderByDesc('id')->first();
+            $latest_signatory_ids = SignatoryV2::where('user_id', Auth::user()->id)
+                ->where('semester_id', $semester->id)
+                ->get(['program_id', 'designee_id', 'semester_id','campus_id','purpose_id'])
+                ->toArray();
+            // dd(SignatoryV2::where('user_id', Auth::user()->id)
+            // ->where('semester_id', $semester->id)
+            // ->get(['program_id', 'designee_id', 'semester_id','campus_id']));
+            $program_ids = array_column($latest_signatory_ids, 'program_id');
+            $designee_ids = array_column($latest_signatory_ids, 'designee_id');
+            $campus_ids = array_column($latest_signatory_ids, 'campus_id');
+            $purpose_ids = array_column($latest_signatory_ids, 'purpose_id');
+
+
+            $signatory_ids = SignatoryV2::whereIn('program_id', $program_ids)->whereIn('designee_id', $designee_ids)->whereIn('campus_id', $campus_ids)
+            // ->whereIn('purpose_id', $purpose_ids)
+                ->get(['id']);
             $programs = SignatoryV2::where('user_id', Auth::user()->id)->distinct('campus_id')->get('campus_id');
 
             $clearance_requests =new ClearanceRequestCollection( ClearanceRequestV2::orderByDesc('approved_at')
+            ->whereIn('signatory_id', $signatory_ids)
             ->where('status', true)
             ->when($request->student, function($inner) use($request){
                 $inner->whereHas('student', function($q) use ($request){
@@ -114,9 +131,8 @@ class CompletedClearanceController extends Controller
                     $q->where('program_id',$request->program);
                 });
             }) 
-            ->whereHas('student.program', function($q) use($programs){
-                $q->whereIn('campus_id', $programs);
-            })  
+            ->whereIn('signatory_id', $signatory_ids)
+            ->where('status', true)
             ->paginate($per_page));
 
             return response()->json([
